3d70a266d6c230f93b99533c16dc59e693a32bf4
Evgeny <biatl0n@yandex.ru>
2022-10-16 20:02:13 +0500

    Корректировка ошибок в calc и engine

--- src/Engine.php
+++ src/Engine.php
@@ -5,8 +5,9 @@ namespace BrainGames\Engine;
 use function cli\line;
 use function cli\prompt;
 
-function gameLogic($gameName)
+function gameLogic(string $gameName)
 {
+    $gameData = [];
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -4,6 +4,7 @@ namespace BrainGames\Games\Calc;
 
 function calc()
 {
+    $gameData = [];
     $gameData['header'] = 'What is the result of the expression?';
 
     for ($i = 0; $i < 3; $i++) {
@@ -11,17 +12,11 @@ function calc()
         $randNum2 = rand(0, 99);
         $sign = randSign();
         $gameData[$i]['question'] = "Question: {$randNum1} {$sign} {$randNum2}";
-        switch ($sign) {
-            case "+":
-                $correctAnswer = $randNum1 + $randNum2;
-                break;
-            case "-":
-                $correctAnswer = $randNum1 - $randNum2;
-                break;
-            case "*":
-                $correctAnswer = $randNum1 * $randNum2;
-                break;
-        }
+        $correctAnswer = match ($sign){
+            "*" => $randNum1 * $randNum2,
+            "+" => $randNum1 + $randNum2,
+            "-" => $randNum1 - $randNum2,
+        };
         $gameData[$i][1] = "{$correctAnswer}";
     }
     return $gameData;
