434d74fcf82f00564ee5fb3dd9b125c1aa4b29f7
Evgeny <biatl0n@yandex.ru>
2022-10-16 21:38:27 +0500

    Ошибки

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -4,13 +4,13 @@ namespace BrainGames\Games\Calc;
 
 function calc()
 {
-    $gameData = [[]];
+    $gameData = [];
     $gameData['header'] = 'What is the result of the expression?';
     for ($i = 0; $i < 3; $i++) {
         $randNum1 = rand(0, 99);
         $randNum2 = rand(0, 99);
         (string)$sign = randSign();
-        $gameData = [$i => ['question' => "Question: {$randNum1} {$sign} {$randNum2}"]];
+        $gameData[$i] = ['question' => "Question: {$randNum1} {$sign} {$randNum2}"];
         if ($sign === '*') {
             $correctAnswer = $randNum1 * $randNum2;
         } elseif ($sign === '+') {
